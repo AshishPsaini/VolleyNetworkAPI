0d4493a07f5518a26d053e390405cb2fa3f40ed2
AshishPsaini <ashishpsaini@gmail.com>
2014-10-10 16:58:36 +0530

    volley android api code

---
+++ src/com/priyashi/volleyapiexample/volley/utils/Const.java
@@ -0,0 +1,10 @@
+package com.priyashi.volleyapiexample.volley.utils;
+
+public class Const {
+	
+	//All data links from here
+	public static final String URL_JSON_OBJECT = "http://api.androidhive.info/volley/person_object.json";
+	public static final String URL_JSON_ARRAY = "http://api.androidhive.info/volley/person_array.json";
+	public static final String URL_STRING_REQ = "http://api.androidhive.info/volley/string_response.html";
+	public static final String URL_IMAGE = "http://www.nlcindia.com/photo/images/swim_jpg.jpg";
+}
